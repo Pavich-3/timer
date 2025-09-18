a70832bddf31c5d089c661e4aa507aae8301423d
Pavlo Baranchuk <pavlo.baranchuk@goreel.tech>
2025-09-18 12:11:46 +0300

    added 200 hertz pwm

--- src/main.cpp
+++ src/main.cpp
@@ -12,7 +12,9 @@ int main(void)
 
 void PWM_Init(void)
 {
-    
+    TCCR1A |= (1 << COM1A1) | (1 << WGM11);
+    TCCR1B |= (1 << WGM13) | (1 << WGM12) | (1 << CS11) | (1 << CS10);
+    ICR1 = 624;   
 }
 
 void TIMER_Init(void)
